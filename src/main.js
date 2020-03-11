51c295877f53ad14f5ac63e35add23dac8d1c5de
avcd <673115816@qq.com>
2020-03-12 00:19:50 +0800

    day1 + day2

--- src/main.js
+++ src/main.js
@@ -2,9 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import BootStrapVue from 'bootstrap-vue'
+import VueChatScroll from 'vue-chat-scroll'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/css/loading.css'
+import './assets/css/loading-btn.css'
 
 Vue.config.productionTip = false
-
+Vue.use(BootStrapVue)
+Vue.use(VueChatScroll)
 new Vue({
   router,
   store,
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Login from '../views/Login'
+import ChatDashboard from '../views/ChatDashboard'
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'login',
+    component: Login
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/chat',
+    name: 'chat',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: ChatDashboard
   }
 ]
 
